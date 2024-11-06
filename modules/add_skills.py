de6f9a18d4699a3c9e3863c08e90a203322b851f
ErnestasZ <ernestas.zemaitis@gmail.com>
2024-11-06 09:03:56 +0200

    add new column paticipants_qty to Lecture

--- modules/add_skills.py
+++ modules/add_skills.py
@@ -2,7 +2,11 @@ from db_classes import Skill
 from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy import create_engine
 
-def add_skill(session: Session, user_id: int, title: str, description: str):
+engine = create_engine("sqlite:///app.db")
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def add_skill(user_id: int, title: str, description: str):
 
     existing_skill = session.query(Skill).filter_by(title=title, user_id=user_id).first()
     if existing_skill:
@@ -17,13 +21,9 @@ def add_skill(session: Session, user_id: int, title: str, description: str):
 
     session.add(new_skill)
     session.commit()
+    session.close()
     print(f"Skill {title} succesfully added")
 
 
-    engine = create_engine("sqlite:///app.db")
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
-    add_skill(session, user_id=2, title="Test skill", description="this is just a test")
-
-    session.close()
+add_skill(user_id=2, title="Test skill", description="this is just a test")
